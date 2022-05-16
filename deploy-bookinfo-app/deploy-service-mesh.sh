668aa64bf3599671766fac01e453e87ac9dc0cf8
Hugo Pfeffer <hugo.pguima@gmail.com>
2022-05-16 14:14:44 -0300

    feat: add labels

--- deploy-bookinfo-app/deploy-service-mesh.sh
+++ deploy-bookinfo-app/deploy-service-mesh.sh
@@ -29,5 +29,16 @@ export GATEWAY_URL=$(oc -n istio-system get route istio-ingressgateway -o jsonpa
 
 oc apply -n bookinfo -f https://raw.githubusercontent.com/Maistra/istio/maistra-2.0/samples/bookinfo/networking/destination-rule-all.yaml
 
+oc label deployment/productpage-v1 app.openshift.io/runtime=python --overwrite
+oc annotate deployment/productpage-v1 app.openshift.io/connects-to=details-v1,reviews-v1,reviews-v2,reviews-v3 --overwrite
+oc label deployment.apps/reviews-v1 app.openshift.io/runtime=java --overwrite
+oc label deployment.apps/reviews-v2 app.openshift.io/runtime=java --overwrite
+oc label deployment.apps/reviews-v3 app.openshift.io/runtime=java --overwrite
+oc label deployment.apps/details-v1 app.openshift.io/runtime=ruby --overwrite
+oc label deployment.apps/ratings-v1 app.openshift.io/runtime=nodejs --overwrite
+oc annotate deployment/reviews-v2 app.openshift.io/connects-to=ratings-v1 --overwrite
+oc annotate deployment/reviews-v3 app.openshift.io/connects-to=ratings-v1 --overwrite
+
+
 echo "################################################################################"
 echo "http://$GATEWAY_URL/productpage"
